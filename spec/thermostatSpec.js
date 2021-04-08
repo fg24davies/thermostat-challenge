20d66eab834de5a86cbcee54f71818757ff1d8a1
Flora Davies <daviesfgo@gmail.com>
2021-04-08 16:00:06 +0100

    Finish 1st spec testing
    Co-authored-by: Stu Behan <StuBehan@users.noreply.github.com>

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -21,7 +21,7 @@ describe('Thermostat', function() {
   });
 
   it('minimum temperature is 10', function() {
-    thermostat.temperature = 10
+    thermostat.temperature = 10;
     thermostat.down();
     expect(thermostat.temperature).toEqual(10);
   });
@@ -29,6 +29,41 @@ describe('Thermostat', function() {
   it('has a powersave mode', function() {
     expect(thermostat.powerSave).toEqual(true);
   });
+
+  it('powersave mode is on max temp is 25', function() {
+    thermostat.temperature = 25;
+    thermostat.up();
+    expect(thermostat.temperature).toEqual(25);
+  });
+
+  it('powersave mode is off max temp is 32', function() {
+    thermostat.powerSaveSwitch();
+    thermostat.temperature = 32;
+    thermostat.up();
+    expect(thermostat.temperature).toEqual(32);
+  });
+
+  it('can change state of powersave', function() {
+    thermostat.powerSaveSwitch();
+    expect(thermostat.powerSave).toEqual(false);
+    thermostat.powerSaveSwitch();
+    expect(thermostat.powerSave).toEqual(true);
+  });
+
+  it('resets the temperature to 20', function() {
+    thermostat.temperature = 25;
+    thermostat.resetTemp();
+    expect(thermostat.temperature).toEqual(20);
+  });
+
+  it('returns the usage of the system at the current temperature', function() {
+    thermostat.temperature = 17
+    expect(thermostat.getUsage()).toEqual('low-usage')
+    thermostat.temperature = 25
+    expect(thermostat.getUsage()).toEqual('medium-usage')
+    thermostat.temperature = 26
+    expect(thermostat.getUsage()).toEqual('high-usage')
+  })
 
 });
 
--- src/thermostat.js
+++ src/thermostat.js
@@ -3,16 +3,42 @@ class Thermostat {
   constructor() {
     this.temperature = 20;
     this.powerSave = true;
+    this.maxTemperature = 25;
   }
 
   up() {
-    this.temperature++
+    if (this.temperature < this.maxTemperature) {
+    this.temperature++;
+    }
   }
 
   down() {
     if (this.temperature > 10) {
-    this.temperature--
+    this.temperature--;
     } 
+  }
+
+  powerSaveSwitch() {
+    this.powerSave = !this.powerSave;
+    if (this.powerSave) {
+      this.maxTemperature = 25;
+    } else {
+      this.maxTemperature = 32;
+    }
+  }
+
+  resetTemp() {
+    this.temperature = 20;
+  }
+
+  getUsage() {
+    if (this.temperature <18) {
+      return 'low-usage';
+    } else if (this.temperature <= 25) {
+      return 'medium-usage';
+    } else {
+      return 'high-usage';
+    }
   }
 }
 
